d43402c534038cfbdbe59df9a14fcbea767b5279
Marian <mail@marian42.de>
2015-10-20 16:48:14 +0200

    Add Planetarium

---
+++ kerbalgit/kerbalgit/GameObjects/CelestialBody.cs
@@ -0,0 +1,12 @@
+﻿
+namespace kerbalgit.GameObjects {
+	public class CelestialBody {
+		public readonly int Id;
+		public readonly string Name;
+
+		public CelestialBody(string name, int id) {
+			Id = id;
+			Name = name;
+		}
+	}
+}
---
+++ kerbalgit/kerbalgit/GameObjects/Planetarium.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace kerbalgit.GameObjects {
+	public class Planetarium {
+		public static Lazy<Planetarium> Instance = new Lazy<Planetarium>();
+
+		public readonly IReadOnlyCollection<CelestialBody> Bodies;
+
+		public Planetarium() {
+			var items = new List<CelestialBody>();
+
+			items.Add(new CelestialBody("Kerbol", 0));
+			items.Add(new CelestialBody("Kerbin", 1));
+			items.Add(new CelestialBody("Moho", 4));
+			items.Add(new CelestialBody("Eve", 5));
+			items.Add(new CelestialBody("Gilly", 13));
+			items.Add(new CelestialBody("Mun", 2));
+			items.Add(new CelestialBody("Minmus", 3));
+			items.Add(new CelestialBody("Duna", 6));
+			items.Add(new CelestialBody("Ike", 7));
+			items.Add(new CelestialBody("Dres", 15));
+			items.Add(new CelestialBody("Jool", 8));
+			items.Add(new CelestialBody("Laythe", 9));
+			items.Add(new CelestialBody("Vall", 10));
+			items.Add(new CelestialBody("Tylo", 12));
+			items.Add(new CelestialBody("Bop", 11));
+			items.Add(new CelestialBody("Pol", 14));
+			items.Add(new CelestialBody("Eelo", 16));
+
+			Bodies = items.AsReadOnly();
+		}
+
+		public static CelestialBody ById(int id) {
+			return Instance.Value.Bodies.First(body => body.Id == id);
+		}
+	}
+}
