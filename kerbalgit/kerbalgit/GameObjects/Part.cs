ce275fa6109f6c6ffb8a6a31578528259343fa48
Marian <mail@marian42.de>
2015-10-20 16:17:03 +0200

    Classes for parts and vessels

---
+++ kerbalgit/kerbalgit/GameObjects/Part.cs
@@ -0,0 +1,32 @@
+﻿using kerbalgit.Tree;
+using System;
+
+namespace kerbalgit.GameObjects {
+	public class Part {
+		public readonly Node node;
+		public readonly Vessel vessel;
+
+		public Part(Node node, Vessel vessel) {
+			if (node.Name != "part") {
+				throw new InvalidOperationException("Node is not a part.");
+			}
+
+			this.node = node;
+			this.vessel = vessel;
+		}
+
+		public long Id {
+			get {
+				return node.GetInt("uid");
+			}
+		}
+
+		public override int GetHashCode() {
+			return Id.GetHashCode();
+		}
+
+		public override bool Equals(object obj) {
+			return obj is Part && (obj as Part).Id == Id;
+		}
+	}
+}
---
+++ kerbalgit/kerbalgit/GameObjects/Vessel.cs
@@ -0,0 +1,39 @@
+﻿using kerbalgit.Tree;
+using System;
+using System.Collections.Generic;
+
+namespace kerbalgit.GameObjects {
+	public class Vessel {
+		public readonly Node Node;
+		public readonly List<Part> parts;
+
+		public Vessel(Node node) {
+			if (node.Name != "vessel") {
+				throw new InvalidOperationException("Node is not a vessel.");
+			}
+
+			this.Node = node;
+			this.parts = new List<Part>();
+		}
+
+		public string Id {
+			get {
+				return Node.GetValue("pid");
+			}
+		}
+
+		public string Name {
+			get {
+				return Node.GetValue("name");
+			}
+		}
+
+		public override int GetHashCode() {
+			return Id.GetHashCode();
+		}
+
+		public override bool Equals(object obj) {
+			return obj is Vessel && (obj as Vessel).Id == Id;
+		}
+	}
+}
