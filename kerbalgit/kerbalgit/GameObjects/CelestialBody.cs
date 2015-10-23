94efe5f5cb685ccb5075639aae295818b3053ab8
Marian <mail@marian42.de>
2015-10-23 19:42:04 +0200

    More detailed planetarium

--- kerbalgit/kerbalgit/GameObjects/CelestialBody.cs
+++ kerbalgit/kerbalgit/GameObjects/CelestialBody.cs
@@ -4,9 +4,19 @@ namespace kerbalgit.GameObjects {
 		public readonly int Id;
 		public readonly string Name;
 
-		public CelestialBody(string name, int id) {
+		public readonly double Radius;
+		public readonly double EdgeOfAtmosphere;
+		public readonly double EdgeOfLowSpace;
+		public readonly double SphereOfInfluence;
+
+		public CelestialBody(string name, int id, double radius, double edgeOfAtmosphere, double edgeOfLowSpace, double sphereOfInfluence) {
 			Id = id;
 			Name = name;
+
+			Radius = radius;
+			EdgeOfAtmosphere = edgeOfAtmosphere;
+			EdgeOfLowSpace = edgeOfLowSpace;
+			SphereOfInfluence = sphereOfInfluence;
 		}
 	}
 }
--- kerbalgit/kerbalgit/GameObjects/Planetarium.cs
+++ kerbalgit/kerbalgit/GameObjects/Planetarium.cs
@@ -11,23 +11,23 @@ namespace kerbalgit.GameObjects {
 		public Planetarium() {
 			var items = new List<CelestialBody>();
 
-			items.Add(new CelestialBody("Kerbol", 0));
-			items.Add(new CelestialBody("Kerbin", 1));
-			items.Add(new CelestialBody("Moho", 4));
-			items.Add(new CelestialBody("Eve", 5));
-			items.Add(new CelestialBody("Gilly", 13));
-			items.Add(new CelestialBody("Mun", 2));
-			items.Add(new CelestialBody("Minmus", 3));
-			items.Add(new CelestialBody("Duna", 6));
-			items.Add(new CelestialBody("Ike", 7));
-			items.Add(new CelestialBody("Dres", 15));
-			items.Add(new CelestialBody("Jool", 8));
-			items.Add(new CelestialBody("Laythe", 9));
-			items.Add(new CelestialBody("Vall", 10));
-			items.Add(new CelestialBody("Tylo", 12));
-			items.Add(new CelestialBody("Bop", 11));
-			items.Add(new CelestialBody("Pol", 14));
-			items.Add(new CelestialBody("Eelo", 16));
+			items.Add(new CelestialBody("Kerbol", 0, 261600000, 0, 1000000000, double.PositiveInfinity));
+			items.Add(new CelestialBody("Kerbin", 1, 600000, 70000, 250000, 84159286));
+			items.Add(new CelestialBody("Moho", 4, 250000, 0, 80000, 9646663));
+			items.Add(new CelestialBody("Eve", 5, 700000, 90000, 400000, 85109365));
+			items.Add(new CelestialBody("Gilly", 13, 13000, 0, 6000,  126123));
+			items.Add(new CelestialBody("Mun", 2, 200000, 0, 60000,  2429559));
+			items.Add(new CelestialBody("Minmus", 3, 60000, 0, 30000, 2247428));
+			items.Add(new CelestialBody("Duna", 6, 320000, 50000, 140000, 47921949));
+			items.Add(new CelestialBody("Ike", 7, 130000, 0, 50000, 1049599));
+			items.Add(new CelestialBody("Dres", 15, 138000, 0, 25000, 32832840));
+			items.Add(new CelestialBody("Jool", 8, 6000000, 200000, 4000000, 2455985185));
+			items.Add(new CelestialBody("Laythe", 9, 500000, 50000, 200000, 3723646));
+			items.Add(new CelestialBody("Vall", 10, 300000, 0, 90000, 2406401));
+			items.Add(new CelestialBody("Tylo", 12, 600000, 0, 250000, 10856518));
+			items.Add(new CelestialBody("Bop", 11, 65000, 0, 25000, 1221061));
+			items.Add(new CelestialBody("Pol", 14, 44000, 0, 22000, 1042139));
+			items.Add(new CelestialBody("Eelo", 16, 210000, 0, 60000, 119082942));
 
 			Bodies = items.AsReadOnly();
 		}
